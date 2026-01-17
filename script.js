c1b4c2eef77223c9d0c56c024854332da9323f4d
Steven <79778284+hkstevenliu04@users.noreply.github.com>
2026-01-17 23:35:33 +0800

    fix: Simplify scrollbar to always show but dim when not scrolling

--- script.js
+++ script.js
@@ -349,14 +349,11 @@ function editSiteDescription() {
 // Setup scroll fade effect for content containers
 function setupScrollFadeEffect() {
     const contentContainers = document.querySelectorAll('.group-content-container');
-    console.log('Setting up scroll fade for', contentContainers.length, 'containers');
     
-    contentContainers.forEach((container, idx) => {
-        console.log('Container', idx, '- scrollHeight:', container.scrollHeight, 'clientHeight:', container.clientHeight);
+    contentContainers.forEach((container) => {
         let scrollTimeout;
         
         container.addEventListener('scroll', () => {
-            console.log('Scroll event fired on container', idx);
             // Add scrolling class to show scrollbar
             container.classList.add('scrolling');
             
@@ -365,7 +362,6 @@ function setupScrollFadeEffect() {
             
             // Set timeout to remove scrolling class after 2 seconds to allow fade transition
             scrollTimeout = setTimeout(() => {
-                console.log('Removing scrolling class from container', idx);
                 container.classList.remove('scrolling');
             }, 2000);
         });
